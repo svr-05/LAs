5dbf963e59f71b44ec62c364c0968ff6fcec0f56
svr-05 <svargasr05@gmail.com>
2025-02-23 14:28:11 -0700

    Song class

---
+++ src/database/store/Song.java
@@ -0,0 +1,46 @@
+package database.store;
+
+public class Song {
+	// instance variables
+	private String title,
+	               author;
+	private String album;
+	private String rating;
+	private boolean isFavorite;
+	
+	// constructor
+	public Song(String title, String author, String album) {
+		this.title = title;
+		this.author = author;
+		this.album = album;
+		this.rating = null;
+		this.isFavorite = false;
+	}
+	
+	// getters
+	public String getTitle() { return this.title; }
+	
+	public String getAuthor() { return this.author; }
+	
+	public String getAlbum() { return this.album; }
+	
+	public String getRating() { return this.rating; }
+	
+	public boolean favoriteStatus() { return this.isFavorite; }
+	
+	// setters
+	public void setFavorite() { this.isFavorite = true; }
+	
+	public void rate(String r) {
+		this.rating = r;
+		if (r == "5") this.isFavorite = true;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Song title: %s, Author: %s, Album: %s",
+				             this.title, this.author, this.album);
+	}
+	
+
+}
--- src/task1/HelloTask.java
+++
@@ -1,8 +0,0 @@
-package task1;
-
-public class HelloTask {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-
-}
