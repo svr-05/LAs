5fc63f92506010cae05343309a81da006f7e0ba8
Timer0491 <tyrikhardley@arizona.edu>
2025-03-21 22:47:43 -0700

    Update SongDataLA2.java

--- src/database/store/SongData.java
+++ src/database/store/SongData.java
@@ -1,34 +1,50 @@
-package database.store;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+package model;
 
 public class SongData {
 	// instance variables
-	public final ArrayList<String> RATINGS;
+	public enum ratingScale { 
+		NONE("Not Rated Yet"),
+		ONE("★"),
+	    TWO("★ ★"),
+	    THREE("★ ★ ★"),
+	    FOUR("★ ★ ★ ★"),
+	    FIVE("★ ★ ★ ★ ★");
+		
+		private final String r;
+
+		ratingScale(String r) {
+			this.r = r;
+		} 
+		
+		@Override
+		public String toString() { // the toString is the "bridge" between the enums and the stars
+			return this.r;
+		}
+	};
 	
 	private String title,
 	               author,
-	               album,
-	               rating;
+	               album;
 	private boolean isFavorite;
+	private ratingScale rating;
 	
 	// constructor
+	/* 
+	 * @pre title != null && author != null && album != null 
+	 */
 	public SongData(String title, String author, String album) {
 		this.title = title;
 		this.author = author;
 		this.album = album;
-		RATINGS = new ArrayList<String>(Arrays.asList
-				(null, "★", "★ ★", "★ ★ ★", "★ ★ ★ ★", "★ ★ ★ ★ ★"));
-		Collections.unmodifiableList(RATINGS); // makes RATING immutable, throws UnsupportedOperationException
-		this.rating = RATINGS.get(0);
+		this.rating = ratingScale.NONE;
 		this.isFavorite = false;
 	}
 	
 	// copy constructor
 	public SongData(SongData song) {
 		this(song.title, song.author, song.album);
+		this.rating = song.rating;
+		this.isFavorite = song.isFavorite;
 	}
 	
 	// getters
@@ -38,9 +54,7 @@ public class SongData {
 	
 	public String getAlbum() { return this.album; }
 	
-	public String getRating() {
-		return this.rating;
-	}
+	public ratingScale getRating() { return this.rating; }
 
 	
   public Song getSongObject() {
@@ -53,15 +67,36 @@ public class SongData {
 	public void setFavorite() { this.isFavorite = true; }
 	
 	public void rate(int r) {
-    if(r == 5) setFavorite();
-		this.rating = RATINGS.get(r);
+		if (r < 1 || r > 5) {
+			System.out.println("Please Enter an integer between 1 and 5!");
+			return;
+		}
+		switch (r) {
+			case 1:
+				this.rating = ratingScale.ONE;
+				break;
+			case 2:
+				this.rating = ratingScale.TWO;
+				break;
+			case 3:
+				this.rating = ratingScale.THREE;
+				break;
+			case 4:
+				this.rating = ratingScale.FOUR;
+				break;
+			case 5:
+				this.rating = ratingScale.FIVE;
+				this.isFavorite = true;
+				break;
+		}
+		System.out.println("Rating saved!");
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("Song title: %s, Author: %s, Album: %s",
-				             this.title, this.author, this.album);
+		String fav = (this.isFavorite) ? "Favorite" : "Not Favorite";
+		return String.format("Song title: %s, Author: %s, Album: %s, Rating: %s, %s",
+				             this.title, this.author, this.album, this.rating, fav);
 	}
-	
 
 }
