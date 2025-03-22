bc6176aaa91f07c146207f19271c7fc292193a76
Timer0491 <tyrikhardley@arizona.edu>
2025-03-21 22:49:08 -0700

    Update SongLA2.java

--- src/database/store/Song.java
+++ src/database/store/Song.java
@@ -1,4 +1,4 @@
-package LA1;
+package model;
 
 public class Song {
 	private String title,
@@ -6,6 +6,9 @@ public class Song {
 	               album;
 	
 	// constructor
+	/* 
+	 * @pre title != null && author != null && album != null 
+	 */
 	public Song(String title, String author, String album) {
 		this.title = title;
 		this.author = author;
@@ -13,7 +16,7 @@ public class Song {
 	}
 	
 	// copy constructor
-	public Song(Song song) { // Need to change this to take in SongData
+	public Song(Song song) {
 		this(song.title, song.author, song.album);
 	}
 	
@@ -23,6 +26,26 @@ public class Song {
 	public String getAuthor() { return this.author; }
 	
 	public String getAlbum() { return this.album; }
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == null) return false;
+		
+		if (o == this) return true;
+		
+		else if (o.getClass() != getClass()) return false;
+		
+		else {
+			return this.title.equals(((Song) o).title) &&  
+		           this.author.equals(((Song) o).author) && 
+		           this.album.equals(((Song) o).album); 
+		}
+	}
+	
+	@Override
+	public int hashCode() {
+		return title.hashCode() + author.hashCode() + album.hashCode();
+	}
 	
 	@Override
 	public String toString() {
